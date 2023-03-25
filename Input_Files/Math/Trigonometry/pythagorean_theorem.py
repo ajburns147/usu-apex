1e6eafb8d193d57665d1e525b1d94dc512bd9d73
FeMan <aj501173@gmail.com>
2023-03-24 21:09:11 -0600

    Updated Py theorem file to fix import bug and make look nicer

--- Input_Files/Math/Trigonometry/pythagorean_theorem.py
+++ Input_Files/Math/Trigonometry/pythagorean_theorem.py
@@ -1,10 +1,11 @@
 from src.TopicFactory import TopicFactory
 
-class Pyhagoreantheorem(TopicFactory):
+class pythagorean_theorem(TopicFactory):
     def __init__(self):
-        self.input_dict = {"a": ["", "", 3],
-                      "b": ["", "", 4],
-                      "c": ["", "", 5],
+        self.input_dict = {
+            "a": ["", "", 3],
+            "b": ["", "", 4],
+            "c": ["", "", 5],
                       }
 
         self.info = {
@@ -13,10 +14,8 @@ class Pyhagoreantheorem(TopicFactory):
              "Note": "This is Pythagoras's theorem",
              "solve_method": "",
              "plot_method": False,
-             "Bonus" : self.Bonus("")
+             "Bonus": self.Bonus("")
              }
-
-
 
     def giveInfo(self):
         return self.info
