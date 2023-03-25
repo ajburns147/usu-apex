5e0d32b223f18c8119e3091a82bfc123c3457c9f
jkarstena <128862647+jkarstena@users.noreply.github.com>
2023-03-24 23:18:58 -0600

    Create Law_of_Cosines.py

---
+++ src/Input_Files/Math/Trigonometry/Law_of_Cosines.py
@@ -0,0 +1,23 @@
+from src.TopicFactory import TopicFactory
+
+
+class Law_of_Cosines(TopicFactory):
+    def __init__(self):
+        self.input_dict = {
+            "a": ["", "", 3],
+            "b": ["", "", 4],
+            "c": ["", "", 5],
+            "A": ["", "", 5]
+        }
+
+        self.info = {
+             "input": self.input_dict,
+             "formula": "a**2 == b**2 + c**2 - 2 * b * c * cos(A)",
+             "Note": "This is the law of cosines",
+             "solve_method": "",
+             "plot_method": False,
+             "Bonus": self.Bonus("")
+             }
+
+    def giveInfo(self):
+        return self.info
