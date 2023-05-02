16b5f926b32e18a5044ddebb7726b5cd2f36420b
FeMan <aj501173@gmail.com>
2023-05-02 14:22:14 -0600

    Created unit test to ensure there is an __init__.py file in every needed directory

---
+++ tests/check_init.py
@@ -0,0 +1,23 @@
+import os
+
+import unittest
+
+
+class TestInputFiles(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.input_dir = "../src/apex/Input_Files"
+
+    def test_check_init_files(self):
+        for root, dirs, files in os.walk(self.input_dir):
+
+            # skip __pycache__ folder
+            if os.path.basename(root) == "__pycache__":
+                continue
+
+            with self.subTest(directory=root):
+                self.assertIn("__init__.py", files, f"{root=} missing __init__.py file")
+
+
+
