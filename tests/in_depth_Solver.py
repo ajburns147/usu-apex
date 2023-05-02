4edcd8a704c0c8fec14c13da33b203b94ca3c6c5
FeMan <aj501173@gmail.com>
2023-05-02 17:45:07 -0600

    changed test to not mutate current status of self.info

--- tests/in_depth_Solver.py
+++ tests/in_depth_Solver.py
@@ -1,5 +1,6 @@
 from apex.Helper import Solver
 from apex.Input_Files.Math.Trigonometry.Pythagorean_Theorem import Pythagorean_Theorem
+from copy import deepcopy
 
 import unittest
 
@@ -22,15 +23,17 @@ class TestSolver(unittest.TestCase):
             "sin(4)",
         ]
 
-    def test_no_runtime_errors(self):
+    def test_solver_fuzzer(self):
         Solver.solve(self.info)
+
+        info = deepcopy(self.info)
 
         for a in self.example_input:
             for b in self.example_input:
                 for c in self.example_input:
-                    self.info["input"]["a"]["value"] = a
-                    self.info["input"]["b"]["value"] = b
-                    self.info["input"]["c"]["value"] = c
+                    info["input"]["a"]["value"] = a
+                    info["input"]["b"]["value"] = b
+                    info["input"]["c"]["value"] = c
 
                     with self.subTest(f"Fuzz testing for {a=} {b=} {c=}"):
-                        Solver.solve(self.info)
+                        Solver.solve(info)
